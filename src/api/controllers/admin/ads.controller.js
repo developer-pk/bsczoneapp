80d7c0bcefadda2a20cdefa1300c77d4cfd60dfa
Manager Singh <managersingh@outlook.com>
2021-12-13 12:20:39 +0530

    =response message updated

--- src/api/controllers/admin/ads.controller.js
+++ src/api/controllers/admin/ads.controller.js
@@ -13,7 +13,8 @@ const Ads = require('../../models/admin/ads.model');
       const ads = new Ads(Object.assign({ createdBy: req.user._id,ads:req.file.filename },req.body));
       const savedAds = await ads.save();
       res.status(httpStatus.CREATED);
-      res.json(savedAds.transform());
+      res.status(httpStatus.CREATED);
+      res.json({"message":"Ad Created Successfuly","data":savedAds.transform()});
     } catch (error) {
       next(Ads.checkDuplicateHobby(error));
     }
@@ -27,6 +28,7 @@ exports.list = async (req, res, next) => {
   try {
     const ads = await Ads.list(req.query);
     const transformedads = ads.map((ads) => ads.transform());
+    res.status(httpStatus.OK);
     res.json(transformedads);
   } catch (error) {
     next(error);
@@ -39,7 +41,13 @@ exports.list = async (req, res, next) => {
  exports.remove = async (req, res, next) => {
   try {
    const ads = await Ads.findByIdAndDelete(req.params.adsId);
-   res.status(httpStatus.NO_CONTENT).end();
+   res.status(httpStatus.OK);
+   if(ads===null){
+    res.json({"message":"Ad already deleted"});
+   }else{
+    res.json({"message":"Ad deleted Successfuly","data":ads});
+   }
+   
   } catch (error) {
     next(error);
   } 
